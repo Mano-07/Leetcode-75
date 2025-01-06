6eda7ad7d3f3db8fa5b527d2ba7d377c79a907a0
Mano-07 <143011834+Mano-07@users.noreply.github.com>
2025-01-06 21:33:39 +0530

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -25,3 +25,19 @@ class Solution {
         return sb.toString();
     }
 }
+
+// 53. Maximum SubArray
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
